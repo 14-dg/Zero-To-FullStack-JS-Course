7e87308d4c73442d1f6a1f4758390ed5811dd64f
Daniel <daniel.graef14@gmail.com>
2024-04-29 21:55:33 +0200

    5 Getting And Setting Attributes exercise

--- 03. DOM/Finished/5 Getting And Setting Attributes/exercise/app.js
+++ 03. DOM/Finished/5 Getting And Setting Attributes/exercise/app.js
@@ -6,9 +6,7 @@
 const a = document.querySelector("a");
 console.log(a.getAttribute("href"));
 
-let a2 = document.querySelector(".a-two");
-a2.setAttribute("href", "https://www.udemy.com/user/huxn-webdev");
-console.log(a2);
-
-// Bonuse
-console.log(a2.getAttribute("href"));
+const aTwo = document.querySelector(".a-two");
+aTwo.setAttribute("href", "http://example.com");
+console.log(aTwo.getAttribute("href"));
+console.log(aTwo);
