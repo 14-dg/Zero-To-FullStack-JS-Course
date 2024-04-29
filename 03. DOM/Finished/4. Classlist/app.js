7802919643df8cbfda2130567c82a31a9b39dbb5
Daniel <daniel.graef14@gmail.com>
2024-04-29 21:49:01 +0200

    4. ClassList exercise

--- 03. DOM/Finished/4. Classlist/app.js
+++ 03. DOM/Finished/4. Classlist/app.js
@@ -10,6 +10,13 @@ console.log(h1.classList);
 // console.log((h1.style.fontWeight = "normal"));
 
 // In EDITOR
-h1.classList.add("styles");
+h1.classList.add("strong");
+console.log(h1.classList);
 h1.classList.remove("styles");
+console.log(h1.classList);
 h1.classList.toggle("styles");
+console.log(h1.classList);
+
+h1.style.color = "purple";
+h1.style.backgroundColor = "skyblue";
+h1.style.fontSize = "100px";
